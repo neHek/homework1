5c5936d2ae3a9bbafb0d21a6589b4cf7d3ad3ad6
neHek <kokarev97@inbox.ru>
2017-09-20 22:44:09 +0300

    Немного изящнее

--- Простые числа.py
+++ Простые числа.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
 
 k=int(input("Число: ")) # Пользователь вводит число
-simp_numb=[1,2,3] # Мы знаем, что они простые
-for i in range(4,k): # Проходим по всем числам от 4 до пользовательского
-    for j in range(1,len(simp_numb)): # Делим на каждое простое число
-        if i % simp_numb[j]==0: # Делится - перехолдим к следующему
+simp_numb=[2,3] # Мы знаем, что они простые
+for i in range(4,k+1): # Проходим по всем числам от 4 до пользовательского
+    for j in simp_numb: # Делим на каждое простое число
+        if i % j==0: # Делится - переходим к следующему
             break
-        elif (i%simp_numb[j]!=0) and (j+1==len(simp_numb)): # Не делится - добавляем в список
-            simp_numb.append(i)
+    else:
+        simp_numb.append(i)
 
 print(simp_numb)
 
